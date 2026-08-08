70fe12c3728f42e229d60a118e17f813a5f23607
HarshadityaGollavilli <135010546+HarshadityaGollavilli@users.noreply.github.com>
2026-08-08 17:16:44 +0530

    Suggestion table created

--- app.py
+++ app.py
@@ -598,7 +598,8 @@ def user_suggestions():
     return render_template('dashboard.html')
 
 
+with app.app_context():
+    db.create_all()
+    
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
